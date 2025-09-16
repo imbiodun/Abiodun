750a727d34765d596d5c8e8bd56e32b5f018b080
Abiodun <akinrinolaabiodun16@gmail.com>
2025-09-17 00:04:40 +0300

    Create basic_operations.py

---
+++ basic_operations.py
@@ -0,0 +1,6 @@
+number1= 10
+number2=5 
+sum= number1 + number2
+print(f'sum of {number1} and {number2} is {sum}')
+difference= number1 - number2
+print(f'difference between  {number1} and {number2} is {difference}')
