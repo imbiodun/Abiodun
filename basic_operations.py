fd82529cd27aa25ed133e81f814d90abd0810492
Akinrinola Abiodun <akinrinolaabiodun16@gmail.com>
2026-01-02 10:21:10 +0200

    Delete basic_operations.py

--- basic_operations.py
+++
@@ -1,8 +0,0 @@
-number1= 10
-number2=5 
-sum= number1 + number2
-print(f'sum of {number1} and {number2} is {sum}')
-difference= number1 - number2
-print(f'difference between  {number1} and {number2} is {difference}')
-product= number1 * number2
-print(f'product of {number1} and {number2} is {product}')
